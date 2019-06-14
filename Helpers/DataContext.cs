ebf7d0bb63909b468c82d60770f3991c9d7dad9b
DESKTOP-C72EGO5\fthcn <fthcnkrtrn@gmail.com>
2019-06-15 00:20:16 +0300

    init

---
+++ Helpers/DataContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+namespace WebApi.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //  optionsBuilder.UseSqlServer(@"Server=13.59.157.144; Database=FoodInfoService; User Id=fiservice;Password=foodinfoservice;MultipleActiveResultSets:true");
+               // optionsBuilder.UseSqlServer(@"Server=DESKTOP-S44KKNG; Database=Ecommerce; Trusted_Connection=True; MultipleActiveResultSets= True;");
+                optionsBuilder.UseSqlServer(@"Server=FATIH; Database=Ecommerce; Trusted_Connection=True; MultipleActiveResultSets= True;");
+
+
+            }
+
+        }
+    }
+
+}
